320da4e7dbfd26950f138b38f948dad95aee1f65
xuhui <xuhui@simpletour.com>
2016-04-19 16:05:28 +0800

    退款规则Domain，dao，biz接口

---
+++ sale/dao/src/main/java/com/simpletour/dao/sale/IRefundPolicyDao.java
@@ -0,0 +1,11 @@
+package com.simpletour.dao.sale;
+
+import com.simpletour.commons.data.dao.IBaseDao;
+
+/**
+ * User: XuHui/xuhui@simpletour.com
+ * Date: 2016/4/19
+ * Time: 15:04
+ */
+public interface IRefundPolicyDao extends IBaseDao{
+}
---
+++ sale/dao/src/main/java/com/simpletour/dao/sale/imp/RefundPolicyDaoImp.java
@@ -0,0 +1,13 @@
+package com.simpletour.dao.sale.imp;
+
+import com.simpletour.commons.data.dao.IBaseDao;
+import com.simpletour.commons.data.dao.jpa.DependencyHandleDAO;
+import com.simpletour.dao.sale.IRefundPolicyDao;
+
+/**
+ * User: XuHui/xuhui@simpletour.com
+ * Date: 2016/4/19
+ * Time: 15:05
+ */
+public class RefundPolicyDaoImp extends DependencyHandleDAO implements IRefundPolicyDao, IBaseDao {
+}
